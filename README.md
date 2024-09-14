0c31536da7fbd1a31fa2bb3722da59a8cb5a1502
Antonio-WebLab <antonio.web.lab@gmail.com>
2024-09-14 22:02:18 +0100

    creo repo per git e github

---
+++ README.md
@@ -0,0 +1,23 @@
+# Libro : automate the Borring Staffs with Python
+
+Alberto ha creato una repository in GitHub ma per il momento è vuota.
+Ho inserito la repo. nei preferiti del mio github.
+Abbiamo abbandonato lo studio del libro per iniziare Typescript.
+
+## Cosa ho visto:
+Del libro ho già anche letto il cap. sui files JSON e Selenium che non ha funzionato.
+
+
+
+<br>
+<br>
+
+# Preparazione ambiente Locale
+## Terminale:  
+## per VSC :
+
+### miei helpers per file.md
+
+<code></code>
+<br>
+<br>
---
+++ chapter_1.py
@@ -0,0 +1,47 @@
+# Chapter 1
+
+# Answers to:
+# quest. 1
+# * - / +  are operators and others else are values.
+# quest. 2
+# spam is a variable 'spam' is a string.
+# quest. 3
+# integers, floating-point number, strings.
+# quest. 4
+# an expression is made up operators and operants, expressions are used to give istructions to the program language.
+# quest. 5
+# statement is a box that contain value and its value can change , expression is a group of values.
+# quest. 6
+# 21
+# quest. 7
+# 'spamspamspam'
+# 'spamspamspam'
+# quest. 8
+# eggs is valid bcos is a string, 100 is invalid bcouse is a integer. , should be _100 or x100 for example.
+# quest. 9
+# int() , float() , str()
+# quest. 10
+# bcos 99 is an integer literal and should be string literal like '99', it should be typed with : str(99) or '99' .
+# (ndAnt. mizzica 99 burritos alla salute Tua !!!)
+
+# Extra credit:
+
+# len()
+# is build-in function, return the number of chars as a int value , only if the parameter is not an int or float literals. String, variable, list, dict, etc.
+# len(111) >>> TypeError: object of type 'int' has no len()
+# len ('111') 
+# >>> 3
+# x = '1111'
+# y = len (x)
+# y 
+# >>> 4
+
+# round()
+# is build-in function too, takes 2 arguments (parameters) round( 'float' , 'how many digit return')
+# ex. 
+# round(100.111 , 3) 
+# >>> 100.111
+# round(100.11 , 2) 
+# >>> 100.11
+# round(100.1 , 1) 
+# >>> 100.1
